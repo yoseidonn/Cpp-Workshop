99d04d1cd130c6fdb25b6b149549d6666f53be5c
yoseidonn <yusufkiris.py@gmail.com>
2024-11-15 21:36:39 +0300

    Create merge_sort() and add into program

--- main.cpp
+++ main.cpp
@@ -4,10 +4,12 @@
 #include "limits"
 
 #include "custom_lib.h"
+#include "merge_sort.h"
 // Incude other libs to perform operations
 
 using namespace std;
 using customlib::contains;
+using customlib::is_numeric;
 using customlib::get_validated_int;
 
 
@@ -25,6 +27,8 @@ const string MENU_MSG =             "Merhabalar, bu program içerisinde barınd�
                                     "|-->Search Algorithms\n"
                                     "|\t|--> (6) Binary Search\n\n"
                                     "Çıkış için (0)\n";
+
+void perform_merge_sort();
 
 int main(){
     vector<int> operations = {0, 1, 2, 3, 4, 5, 6};
@@ -61,6 +65,7 @@ int main(){
                 break;
             case 5:
                 cout << "İşlem 5" << endl << endl;
+                perform_merge_sort();
                 break;
             case 6:
                 cout << "İşlem 6" << endl << endl;
@@ -70,4 +75,30 @@ int main(){
         }
     }
     return 0;
+}
+
+void perform_merge_sort() {
+    int i=0, num;
+    string input;
+    vector<int> arr;
+
+    cout << "Bu algoritma <int> modunda çalışacaktır! (q: sonlandır)" << endl;
+    while (true){
+        printf("Dizi[%d]: ", i+1);
+        cin >> input;
+        
+        if (input == "q") break;
+        
+        if (!is_numeric(input))
+            cout << "Lütfen sayısal bir değer giriniz!";
+        else
+            arr.push_back(stoi(input));
+        
+        ++i;
+    }
+
+    arr = merge_sort(arr);
+
+    for(auto i:arr) printf("%d, ", i);
+    cout << endl;
 }
--- sorting/merge_sort.h
+++ sorting/merge_sort.h
@@ -2,7 +2,67 @@
 #define MERGE_SORT
 
 #include "iostream"
+#include "custom_lib.h"
 
 using namespace std;
+
+
+template <typename T>
+vector<T> merge(vector<T> leftArr, vector<T> rightArr) {
+    vector<T> newArr;
+    int i, j;
+    i = j = 0;
+    
+    while(i<leftArr.size() && j<rightArr.size()) {
+        if (leftArr[i] < rightArr[j]) {
+            newArr.push_back(leftArr[i]);
+            ++i;
+        } else {
+            newArr.push_back(rightArr[j]);
+            ++j;
+        }
+    }
+    
+    while (i<leftArr.size()) {
+        newArr.push_back(leftArr[i]);
+        ++i;
+    }
+    while (j<rightArr.size()) {
+        newArr.push_back(rightArr[j]);
+        ++j;
+    }
+
+    return newArr;
+}
+
+template <typename T>
+vector<T> merge_sort(vector<T> arr) {
+    vector<T> leftArr, rightArr;
+    int s, e, mid;
+    s = 0, e = arr.size()-1, mid = (s+e)/2;
+    
+    if (arr.size() > 2) {
+        for(int i=s; i<mid; ++i)
+            leftArr.push_back(arr[i]);
+        for(int i=mid; i<=e; ++i)
+            rightArr.push_back(arr[i]);        
+            
+        leftArr = merge_sort(leftArr);
+        rightArr = merge_sort(rightArr);
+        
+    } else if (arr.size() == 2) {
+        if (arr[0] > arr[1]) {
+            int temp = arr[0];
+            arr[0] = arr[1];
+            arr[1] = temp;
+        }
+        return arr;    
+        
+    } else {
+        return arr;
+    }
+
+    return merge(leftArr, rightArr);
+}
 
 #endif
